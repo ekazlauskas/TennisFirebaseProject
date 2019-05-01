0e585a0b059a85c8e89a5ca97eb4e733761e7ef6
patriotfever4@gmail.com <Ek@Z0463>
2019-04-30 20:11:14 -0500

    Partial Update to Firebase Project

--- app/src/main/java/css/tennisfirebaseproject/MainActivity.java
+++ app/src/main/java/css/tennisfirebaseproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -17,7 +18,13 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
         Button btnAdd, btnDelete, btnDetails;
-        ListView ListViewMathces;
+        ListView ListViewMatches;
+        ArrayAdapter<Tennis> tennisMatchAdapter;
+        List<Tennis> tennisList;
+        DatabaseReference myTennisDbRef;
+        TennisFirebaseData tennisDataSource;
+        TennisMatch tennisSelected;
+
 
 
     @Override
@@ -25,19 +32,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createFirebaseDataChange();
         // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Welcome to Tennis Tracker");
+        //FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //DatabaseReference myRef = database.getReference("Welcome to Tennis Tracker");
 
+    }
 
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+    //This class is designed to help the app data update in real time, this will help
+    //coaches and the head of athletic communications see the results of
+    //matches so that they can update the CSS website.
+    private void createFirebaseDataChange(){
+        tennisDataSource = new TennisFirebaseData();
+        myTennisDbRef = tennisDataSource.open();
+        myTennisDbRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d("VALUE", "Value is: " + value);
+                Log.d("CISMOBILE", "Initalizing onDataChange()");
+                tennisList = tennisDataSource.getallTennisMatch(dataSnapshot);
+                tennisMatchAdapter = new TennisMatchAdapter(MainActivity.this, android.R.layout.simple_list_item_single_choice, tennisList);
+                ListViewMatches.setAdapter(tennisMatchAdapter);
             }
 
             @Override
@@ -47,5 +63,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-}
+
+    //This class is designed to set up the Add Match button on the Mainactivity
+    //so that the user of the application can go to the Add Match Screen to add match details
+    //for the current match being played.
+    private void createAddMatchButton(){
+        //Sets up the button to add a match to the MainActivity using a separate activity
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Starts the Intent to go to the add match activity
+                Intent detailAddIntent = new Intent(view.getcontext(), AddMatchActivity.class);
+                finish();
+                startActivity(detailAddIntent);
+            }}};
+;
+            }
+    }
 
--- app/src/main/java/css/tennisfirebaseproject/TennisMatchAdapter.java
+++ app/src/main/java/css/tennisfirebaseproject/TennisMatchAdapter.java
@@ -2,8 +2,10 @@ package css.tennisfirebaseproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class TennisMatchAdapter extends AppCompatActivity {
+public class TennisMatchAdapter extends ArrayAdapter<Tennis> {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
